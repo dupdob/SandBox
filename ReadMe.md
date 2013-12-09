e73fd7680b3366b281a79fa5c74cc6c488790284
Cyrille Dupuydauby <dupdob@gmail.com>
2013-12-09 07:31:09 +0100

    First commit with code for Can You Spot the Deadlock 4

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+**Can you spot the deadlock** is a collection of code samples
+=============================================================
+
+Overview
+========
+This is a collection of code samples displaying various multhreading oriented issues.
+Those are the bases for issue analysis exercices
+
+
+Technical environement
+======================
+*Tools:
+* Visual Studio 2012
+* Resharper
+
+**Dependencies**:
+* .Net Framework 4.0
+
+
+
---
+++ SandBox/Program.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Cyrille DUPUYDAUBY">
+//   Copyright 2013 Cyrille DUPUYDAUBY
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// <summary>
+//   
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SandBox
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Entry point for the various tests.
+        /// </summary>
+        private static void Main()
+        {
+            var tester = new CYSDL4();
+            
+            tester.Entry();
+        }
+    }
+}
